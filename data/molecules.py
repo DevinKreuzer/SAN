8f4025013cf95ae41d64094299698e37ec82e98c
Hannes Stärk <hannes.staerk@gmail.com>
2021-06-12 17:51:24 +0200

    use eigh instead of eig for faster calculation of eigenvectors and to remove the necessity to sort them as numpy already returns eigvals and eigvecs sorted in ascending order. The Laplacian is symmetric so we can use eigh instead of eig

--- data/molecules.py
+++ data/molecules.py
@@ -127,14 +127,12 @@ def laplace_decomp(g, max_freqs):
     L = sp.eye(g.number_of_nodes()) - N * A * N
 
     # Eigenvectors with numpy
-    EigVals, EigVecs = np.linalg.eig(L.toarray())
-    idx = EigVals.argsort()[0 : max_freqs] # Keep up to the maximum desired number of frequencies
-    EigVals, EigVecs = EigVals[idx], np.real(EigVecs[:,idx])
+    EigVals, EigVecs = np.linalg.eigh(L.toarray())
+    EigVals, EigVecs = EigVals[: max_freqs], EigVecs[:, :max_freqs]  # Keep up to the maximum desired number of frequencies
 
-    #Sort, normalize and pad EigenVectors
-    EigVecs = EigVecs[:, EigVals.argsort()]# increasing order
+    # Normalize and pad EigenVectors
     EigVecs = torch.from_numpy(EigVecs).float()
-    EigVecs= F.normalize(EigVecs, p=2, dim=1, eps=1e-12, out=None)
+    EigVecs = F.normalize(EigVecs, p=2, dim=1, eps=1e-12, out=None)
     
     if n<max_freqs:
         g.ndata['EigVecs'] = F.pad(EigVecs, (0, max_freqs-n), value=float('nan'))
